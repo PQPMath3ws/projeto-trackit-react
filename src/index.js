8b4773fe2e2aee3f6d436399a1f3ecd51835ea93
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-12-15 19:25:57 -0300

    Estilo de reset e estilo global adicionado

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
+
+import Global from "./styles/Global";
+import Reset from "./styles/Reset";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
+        <BrowserRouter>
+            <Reset></Reset>
+            <Global></Global>
+            <Routes>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
---
+++ src/styles/Global.js
@@ -0,0 +1,12 @@
+
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+    html, body {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+export default Global;
