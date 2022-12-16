4d685af48f9ea5f54d228f93e6cd7824c8586382
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-12-16 07:07:15 -0300

    Remoção (gamb) de correção do global

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -1,11 +1,8 @@
 
-import { isMobile } from "react-device-detect";
 import { createGlobalStyle } from "styled-components";
 
 const Global = createGlobalStyle`
     body {
-        ${!isMobile ? "padding-top: 80px;" : ""}
-        ${!isMobile ? "padding-bottom: 750px;" : ""}
         width: 100%;
         height: 100%;
     }
